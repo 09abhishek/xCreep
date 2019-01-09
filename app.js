ffbf006c71642c24df3f46228520d4698f13a191
09abhishek <abhi.gautam@imail.com>
2019-01-09 23:31:04 +0530

    rectified all error in the ejs templates

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     //res.status(404).send('</h1>Page not found</h1>');
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page not found', path: ''});
     // res.end();
 });
 
--- routes/admin.js
+++ routes/admin.js
@@ -8,16 +8,21 @@ const rootDir = require('../utils/path');
 const products = [];
 router.get('/add-product', (req, res, next) => {
   //  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')); 
-   
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html')); 
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+
     // res.end();
 });
 
 router.post('/add-product', (req, res, next) => {
-    const item = req.body;
-    products.push(item);
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+    products.push({ title: req.body.title });
     
+    res.redirect('/');
     // console.log(req.body);
     // const item = req.body;
     // products.push(item);
