66da4659dee1f036d8f5b02df9b5abe7a301f101
09abhishek <abhi.gautam@imail.com>
2019-01-16 22:55:53 +0530

    added CRUD operations on the data

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,24 @@
 const path = require('path');
+
 const express = require('express');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const rootDir = require('../utils/path');
-const adminController = require('../controllers/admin');
-
+// /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
+// /admin/products => GET
 router.get('/products', adminController.getProducts);
 
+// /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-// module.exports = router;
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
+
 module.exports = router;
